452fdc79adc4f4942c6ab4e86c0b29bb9fd98203
The Mechanist <zer0inyourface@gmail.com>
2024-03-21 16:03:55 +0530

    wrote the basic working spider

--- animedb/items.py
+++ animedb/items.py
@@ -4,9 +4,50 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import Compose,TakeFirst
 
+def clean_name(inp):
+    return inp[0].split('Anime:')[1].strip()
+
+def clean_desc(inp):
+    for items in inp:
+        inp[inp.index(items)] = items.strip()
+    
+    text = tuple(inp)
+    desc = ''.join(text)
+
+    return desc
+
+def clean_episodes(inp):
+    for items in inp:
+        inp[inp.index(items)] = items.strip()
+        if items.strip() == '':
+            inp.remove(items)
+
+    return inp    
 
 class AnimedbItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field(
+        input_processor = Compose(clean_name),
+        output_processor = TakeFirst()
+    )
+    start_date = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    end_date = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    tags = scrapy.Field()
+    description = scrapy.Field(
+        output_processor = Compose(clean_desc)
+    )
+    episodes = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    image = scrapy.Field(
+        output_processor = TakeFirst()
+    )
+    additional_tags = scrapy.Field()
+    episode_names = scrapy.Field(
+        input_processor = Compose(clean_episodes)
+    )
--- animedb/spiders/animes.py
+++ animedb/spiders/animes.py
@@ -1,11 +1,63 @@
 import scrapy
-
+from scrapy.loader import ItemLoader
+from ..items import AnimedbItem
 
 class AnimesSpider(scrapy.Spider):
     name = "animes"
-    start_urls = ["https://anidb.net/anime/?h=1&noalias=1&orderby.name=0.1&view=list"]
+    
+    HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Cache-Control": "max-age=0",
+    }
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url="https://anidb.net/anime/?h=1&noalias=1&orderby.name=0.1&view=list",
+            headers=self.HEADERS,
+            callback=self.parse
+        )
+
 
     def parse(self, response):
-        yield{
-            'name':response.xpath('//td/text()').get()
-        }
+        links = response.xpath('//td[@data-label="Title"]/a/@href').getall()
+        for link in links:
+            absolute_link = f"https://anidb.net{link}"
+            yield scrapy.Request(
+                url=absolute_link,
+                headers=self.HEADERS,
+                callback=self.parse_anime
+            )
+
+        next_page = response.xpath('(//li[@class="next"])[1]/a/@href').get()
+        if next_page:
+            link = f"https://anidb.net{next_page}"
+            yield scrapy.Request(
+                url=link,
+                headers=self.HEADERS,
+                callback=self.parse
+            )
+
+    
+    def parse_anime(self, response):
+        l = ItemLoader(item=AnimedbItem(), response=response)
+        l.add_xpath('name','//h1[@class="anime"]/text()')
+        l.add_xpath('start_date','//span[@itemprop="startDate"]/@content')
+        l.add_xpath('end_date','//span[@itemprop="endDate"]/@content')
+        l.add_xpath('tags','//span[@itemprop="genre"]/text()')
+        l.add_xpath('description','//div[@itemprop="description"]/text()')
+        l.add_xpath('episodes','//span[@itemprop="numberOfEpisodes"]/text()')
+        l.add_xpath('image','//div[@class="image"]//img/@src')
+        l.add_xpath('additional_tags','//div[@id="tabbed_pane_main_4"]//span[@class="tagname"]/text()')
+        l.add_xpath('episode_names','//table[@id="eplist"]//td[@class="title name episode"]/label/text()')
+
+        item = l.load_item()
+        yield item
